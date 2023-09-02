62b6eaedaef69f80f8e292dc55416e18bf1f757e
Daisy <daisy.t.gan@gmail.com>
2023-09-01 22:30:42 -0500

    allow word count to display

--- contentScript.js
+++ contentScript.js
@@ -4,58 +4,58 @@ var clientY;
 
 // Listen for messages from the popup
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === "modifyDOM") {
-      const textBoxDiv = document.createElement("div");
-      textBoxDiv.innerHTML = "Hello, I've been added by the extension!";
-      textBoxDiv.setAttribute("id"),
-      textBoxDiv.style.backgroundColor =   "rgba(217, 217, 217,0.85)";
-      textBoxDiv.style.color = "#000";
-      textBoxDiv.style.textAlign = "left";
-        textBoxDiv.style.padding = "6px";
-        textBoxDiv.style.position = "absolute";
-        textBoxDiv.style.width = "auto";
-        textBoxDiv.style.height = "auto";
-        textBoxDiv.style.fontSize = "12px";
-      document.body.append(textBoxDiv);
+  if (message.action === "modifyDOM") {
+    const textBoxDiv = document.createElement("div");
+    //textBoxDiv.innerHTML = "Hello, I've been added by the extension!";
+    textBoxDiv.setAttribute("id","hidepopUp");
+    textBoxDiv.style.backgroundColor =   "rgba(217, 217, 217,0.85)";
+    textBoxDiv.style.color = "#000";
+    textBoxDiv.style.textAlign = "left";
+    textBoxDiv.style.padding = "6px";
+    textBoxDiv.style.position = "absolute";
+    textBoxDiv.style.width = "auto";
+    textBoxDiv.style.height = "auto";
+    textBoxDiv.style.fontSize = "12px";
+    document.body.append(textBoxDiv);
 
-        // **************
-      const wordCount = document.createElement("p");
-    const node = document.createTextNode("This is new.");
+      // **************
+    const wordCount = document.createElement("p");
+    //const node = document.createTextNode("This is new.");
     wordCount.setAttribute("id","wordOutput");
-    wordCount.appendChild(node);
-      textBoxDiv.appendChild(wordCount);
+    //wordCount.appendChild(node);
+    textBoxDiv.appendChild(wordCount);
 
-        //********* */
-      const charCount = document.createElement("p");
-      const node1 = document.createTextNode("This is new.");
-      charCount.setAttribute("id","charOutput");
-      charCount.appendChild(node1);
-      textBoxDiv.appendChild(charCount);
-    }
-    document.addEventListener("mouseup", function(event) {
-      var popup = document.getElementById("hidepopUp");
-      popup.style.display = 'none';
-      clientX = event.pageX;
-      clientY = event.pageY;
+      //********* */
+    const charCount = document.createElement("p");
+    //const node1 = document.createTextNode("This is new.");
+    charCount.setAttribute("id","charOutput");
+    //charCount.appendChild(node1);
+    textBoxDiv.appendChild(charCount);
+  }
+
+
+  document.addEventListener("mouseup", function(event) {
+    var popup = document.getElementById("hidepopUp");
+    console.log("once clicked pop up");
+    popup.style.display = 'none';
+    clientX = event.pageX;
+    clientY = event.pageY;
   });
+
+  let drag = false;
+  document.addEventListener('mousedown', function() {
+      drag = false;
   });
+  document.addEventListener('mousemove', function() {
+      drag = true;
+      //console.log("drag");
+  });
+  document.addEventListener('mouseup', function() {
+      drag ? runTextManipulation() : doNothing();
 
-  /****************************** */
+  });
+});
 
-document.addEventListener("DOMContentLoaded", function(event){
-    let drag = false;
-    document.addEventListener('mousedown', function() {
-        drag = false;
-    });
-    document.addEventListener('mousemove', function() {
-        drag = true;
-        //console.log("drag");
-    });
-    document.addEventListener('mouseup', function() {
-        drag ? runTextManipulation() : doNothing();
-
-    });
- });
 
 function doNothing(){
     return;
@@ -67,14 +67,14 @@ function runTextManipulation(){
     var wordCount = highlightedText.split(/\s+/).filter(Boolean).length;
     var charCount = highlightedText.length;
 
-    console.log("Outside");
-    console.log('Number of words:', wordCount);
-    console.log('Number of characters:', charCount);
+    // console.log("Outside");
+    // console.log('Number of words:', wordCount);
+    // console.log('Number of characters:', charCount);
 
     document.getElementById("wordOutput").innerHTML = "words: " + wordCount;
     document.getElementById("charOutput").innerHTML = "characters: " + charCount;
 
-    console.log("inside");
+    // console.log("inside");
 
     var popup = document.getElementById("hidepopUp");
 
@@ -92,21 +92,38 @@ function runTextManipulation(){
         }
 };
 
+function getHighlightedText() {
+  var text = "";
+  if (window.getSelection) {
+      text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type !== "Control") {
+      text = document.selection.createRange().text;
+  }
+  
+  return text;
+  
+};
+
 // document.addEventListener("mouseup", function(event) {
-//     var popup = document.getElementById("hidepopUp");
-//     popup.style.display = 'none';
+//     // var popup = document.getElementById("hidepopUp");
+//     // popup.style.display = 'none';
 //     clientX = event.pageX;
 //     clientY = event.pageY;
 // });
 
-function getHighlightedText() {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type !== "Control") {
-        text = document.selection.createRange().text;
-    }
-    
-    return text;
-    
-};
+  /****************************** */
+
+// document.addEventListener("DOMContentLoaded", function(event){
+//     let drag = false;
+//     document.addEventListener('mousedown', function() {
+//         drag = false;
+//     });
+//     document.addEventListener('mousemove', function() {
+//         drag = true;
+//         console.log("drag");
+//     });
+//     document.addEventListener('mouseup', function() {
+//         drag ? runTextManipulation() : doNothing();
+
+//     });
+//  });
